263ce40fa2b24b78ee131735a9e4591f0963ab5e
viveklg70 <viveklg70@gmail.com>
2022-06-28 13:13:59 +0000

    Fixed workspace commit

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,7 @@ echo "********************"
 echo "*** Building JAR ***"
 echo "********************"
 
-WORKSPACE=/var/efs/containers/jenkins/workspace/pipeline-docker-maven@script
+WORKSPACE=/var/efs/containers/jenkins/workspace/pipeline-docker-maven
 
 docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
 
