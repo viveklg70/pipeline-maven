630aaab2d697d3681306556eedede084c8636209
viveklg70 <viveklg70@gmail.com>
2022-06-28 14:06:53 +0000

    Resolving pipeline build errors

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -2,12 +2,12 @@
 
 # Copy the new jar to the build location
 
-cp -f /home/k8admin/jenkins/pipeline/java-app/target/*.jar /home/k8admin/jenkins/pipeline/jenkins/build/
+cp -f java-app/target/*.jar jenkins/build/
 
 echo "*****************************"
 echo "*** Building Docker Image ***"
 echo "*****************************"
 
-cd /home/k8admin/jenkins/pipeline/jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
 
  
