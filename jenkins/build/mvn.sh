b34e640fc7e260fdaa3a3ed6e5b595875787e8c8
viveklg70 <viveklg70@gmail.com>
2022-06-29 11:49:50 +0000

    Initial commit-29June22-5:20

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,6 +6,8 @@ echo "********************"
 
 WORKSPACE=/var/efs/containers/jenkins/workspace/pipeline-docker-maven
 
-docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
+docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11  sh -c 'whoami > userFile'"$@"
+
+#docker run --rm -v $WORKSPACE/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
 
 #docker run --rm -v /home/k8admin/jenkins/pipeline/java-app/:/app -v /root/.m2:/root/.m2 -w /app maven:3.8.6-jdk-11 "$@"
