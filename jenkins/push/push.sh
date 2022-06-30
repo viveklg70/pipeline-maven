626ef1d81023c9f6a840d7928909ed7da9055937
viveklg70 <viveklg70@gmail.com>
2022-06-30 12:40:15 +0000

    Working commit

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -17,13 +17,13 @@ docker push viveklg70/simple-webapp:$NEW_BUILD_TAG
 docker push viveklg70/simple-webapp:latest
 
 echo "***** Logging IN to AWS ECR *****"
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 611601100250.dkr.ecr.ap-south-1.amazonaws.com
+#aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 611601100250.dkr.ecr.ap-south-1.amazonaws.com
 
 echo "***** Tagging Image *****"
-docker tag app:$OLD_BUILD_TAG 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:app-$NEW_BUILD_TAG
-docker tag app:$OLD_BUILD_TAG 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:latest
+#docker tag app:$OLD_BUILD_TAG 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:app-$NEW_BUILD_TAG
+#docker tag app:$OLD_BUILD_TAG 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:latest
 
 echo "***** Pushing Image *****"
-docker push 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:app-$NEW_BUILD_TAG
-docker push 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:latest
+#docker push 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:app-$NEW_BUILD_TAG
+#docker push 611601100250.dkr.ecr.ap-south-1.amazonaws.com/avangels-test:latest
 
